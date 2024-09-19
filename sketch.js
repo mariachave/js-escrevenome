af820a4070b739cc7759056e557a1537d91ea4b4
mariachave <maria.chaves10@escola.pr.gov.br>
2024-09-19 08:15:33 -0300

    sketch.js

---
+++ sketch.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(400, 400);
+      background("yellow");
+  }
+  
+  function draw() {
+    rect (mouseX, mouseY,20,25)
+    fill ("rgb(73,73,221)")
+    stroke("white")
+  }
